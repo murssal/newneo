2b3a89e3110c4c8c52445dfae6c2c00bb22e3f62
mm-978 <110142347+mm-978@users.noreply.github.com>
2023-12-19 15:54:54 -0800

    got rid of the slash on vercel link lol

--- newneo/queryTest.js
+++ newneo/queryTest.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "https://newneo.vercel.app/",
+  origin: "https://newneo.vercel.app",
   credentials: true,
 };
 
