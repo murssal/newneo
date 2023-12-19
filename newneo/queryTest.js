856ea5f9543c797a0799bafcc5d108433b1ee92b
mm-978 <110142347+mm-978@users.noreply.github.com>
2023-12-19 15:51:33 -0800

    cors to allow vercel link on render

--- newneo/queryTest.js
+++ newneo/queryTest.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://newneo.vercel.app/",
   credentials: true,
 };
 
