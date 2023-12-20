2c13f8d38afd375e525e924651c92212be3da29c
mm-978 <110142347+mm-978@users.noreply.github.com>
2023-12-19 19:03:47 -0800

    removed cookie parser

--- newneo/queryTest.js
+++ newneo/queryTest.js
@@ -47,7 +47,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "newneo")));
 
 // use cookie-parser middleware
-app.use(cookieParser());
+//app.use(cookieParser());
 
 const sessionStore = new MySQLStore(
   {
